77ec79a8723d53c7201a3b6848d637aae1b56dae
Ruttmann <matthias@ruttmann.name>
2020-02-28 03:14:56 +0100

    styling

--- src/app/base/animation.ts
+++ src/app/base/animation.ts
@@ -10,31 +10,31 @@ export const routeAnimations = trigger("routeAnimations", [
         query('.sizing', animate(700, style({ left : "100%" }))),
     ]),
     transition('base => content', [
-        query('.overlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
+        query('#contentoverlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
 
         group([
             query(baseItemsQuerySelector, [ animate(150, style({ opacity : 0 })) ]),
-            query('.overlay',  [ animate(80, style({ opacity: 0.01 })), animate(250) ]),
+            query('#contentoverlay',  [ animate(80, style({ opacity: 0.01 })), animate(250) ]),
         ]),
     ]),
     transition('content => base', [
-        query('.overlay',  [ animate(150, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
+        query('#contentoverlay',  [ animate(150, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
     ]),
     transition('base => introduction', [
-        query('.welcomeoverlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
+        query('#welcomeoverlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
 
         group([
             query(baseItemsQuerySelector, [ animate(150, style({ opacity : 0 })) ]),
-            query('.welcomeoverlay',  [ animate(80, style({ opacity: 0.01 })), animate(250) ]),
+            query('#welcomeoverlay',  [ animate(80, style({ opacity: 0.01 })), animate(250) ]),
         ]),
     ]),
     transition('introduction => base', [
-        query('.welcomeoverlay',  [ animate(150, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
+        query('#welcomeoverlay',  [ animate(150, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
     ]),
     transition('introduction => content', [
-        query('.overlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
-        query('.welcomeoverlay',  [ animate(250, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
-        query('.overlay',  [ 
+        query('#contentoverlay',  [ style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }) ]),
+        query('#welcomeoverlay',  [ animate(250, style({ opacity : 0, position: 'relative', transform: 'translateX(-10%)' })) ]),
+        query('#contentoverlay',  [ 
             style({ opacity : 0, position: 'relative', transform: 'translateX(10%)' }),
             animate(250),
         ]),
